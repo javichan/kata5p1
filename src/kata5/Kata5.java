678cacc1d2b629483931d0bd9d2dc93b8b6616a2
Javichan <Javichan@192.168.1.106>
2018-11-27 18:15:08 +0000

    v2

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -1,19 +1,20 @@
 package kata5;
-import java.sql.ResultSet;
+ import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Connection;
-
-
- public class Kata5 {
+ import java.sql.Statement;
+ 
+public class Kata5 {
  	public static void main(String[] args) {
 
-	createTable();    
-	}
 
+		createTable();
+	}
+ 
  	private static void createTable() {
- 		String url = "jdbc:sqlite:EmailsDB.db";
+ 		String url = "jdbc:sqlite:Kata5.db";
  		String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
 				+ " ID integer PRIMARY KEY AUTOINCREMENT,\n"
 				+ " Mail text NOT NULL);";
@@ -24,38 +25,40 @@ import java.sql.Connection;
  		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
-	}        
+	}
  	private static Connection connect() {
  		final String url = "jdbc:sqlite:KATA5.db";
 		
-		Connection conn = null;
+ 		Connection conn = null;
 		
-		try {
+ 		try {
 			
-			conn = DriverManager.getConnection(url);
+ 			conn = DriverManager.getConnection(url);
 			
-		} catch (SQLException e) {
+ 		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		
-		return conn;
+ 		return conn;
 	}
  	private static void selectAll() {
 		
-		String sql = "SELECT * FROM PEOPLE";
+ 		String sql = "SELECT * FROM PEOPLE";
 		
-		try (Connection conn = connect();
+ 		try (Connection conn = connect();
 				Statement stmt = conn.createStatement();
 				ResultSet rs = stmt.executeQuery(sql)) {
 			
-			while (rs.next()) {
+ 			while (rs.next()) {
 				System.out.printf("ID: %d | Name: %s | Surname: %s | Department: %s\n",
 						rs.getInt("ID"), rs.getString("Name"),
 						rs.getString("Surname"), rs.getString("Department"));
 			}
 			
-		} catch (SQLException e) {
+ 		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		
-	}
+ 	}
 	
-}
+ }
